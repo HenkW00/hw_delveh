3f09bc57c42115ba514090fc6334a2195df988e3
Henk Wietsma <151195189+HenkW00@users.noreply.github.com>
2024-02-11 04:14:51 +0100

    Added version support

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # HW Scripts - Delete Vehicles
 
 **Description**
-
+A script that allows you to run a carwipe every so often (hours/minutes), this will remove all cars that no one is in to prevent lag. 
+This script does not cause any performance loss.
 
 
 **Author**
--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ game 'gta5'
 
 author 'HenkW'
 description 'A script that allows you to run a carwipe every so often (hours/minutes), this will remove all cars that no one is in to prevent lag. This script does not cause any performance loss.'
-version '1.1.1'
+version '1.1.2'
 
 server_script {
     'config.lua',
